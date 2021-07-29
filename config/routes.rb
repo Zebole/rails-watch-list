731650b7d94dc1b6b3bad201e04e6ebf26e464da
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-07-29 16:24:33 +0400

    Creates a List

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # get 'lists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "lists", to: "lists#index", as: :lists
+  post "lists", to: "lists#create"
+  get "lists/new", to: "lists#new", as: :new_list
   get "lists/:id", to: "lists#show", as: :list
+
 end
