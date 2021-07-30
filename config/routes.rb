0b525d3a0bf7da1cc013ca5d5ed38803af29b27b
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-07-30 12:47:40 +0400

    Creates a Bookmark for list

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # get 'lists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "lists", to: "lists#index", as: :lists
-  post "lists", to: "lists#create"
-  get "lists/new", to: "lists#new", as: :new_list
-  get "lists/:id", to: "lists#show", as: :list
-
+  resources :lists , except: [:edit, :update, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
